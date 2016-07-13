996b06ce177e240db858332dc96d479e9ab7d422
jennybc <jenny@stat.ubc.ca>
2016-07-13 09:50:15 +0200

    beef up the example re: rendering an R script; fixes #23

---
+++ render-r-script-demo.R
@@ -0,0 +1,15 @@
+#' ---
+#' title: "R scripts can be rendered!"
+#' author: "Jenny Bryan"
+#' date: "`r format(Sys.Date())`"
+#' output: github_document
+#' ---
+#' Here's some prose in a very special comment. Let's summarize the built-in
+#' dataset `VADeaths`.
+## here is a regular code comment, that will remain as such
+summary(VADeaths)
+
+#' Here's some more prose. I can use usual markdown syntax to make things
+#' **bold** or *italics*. Let's use an example from the `dotchart()` help to
+#' make a Cleveland dot plot from the `VADeaths` data.
+dotchart(VADeaths, main = "Death Rates in Virginia - 1940")
