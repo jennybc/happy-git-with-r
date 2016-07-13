b8a3de21ca7d65527422f5e9f4c8b5d730668046
jennybc <jenny@stat.ubc.ca>
2016-07-13 09:56:46 +0200

    actual render the demo R script
    Why is the html getting produced?
    
    Why do I have to specify output format in the render call? Why is the YAML not getting the job done?

--- render-r-script-demo.R
+++ render-r-script-demo.R
@@ -4,6 +4,7 @@
 #' date: "`r format(Sys.Date())`"
 #' output: github_document
 #' ---
+#'
 #' Here's some prose in a very special comment. Let's summarize the built-in
 #' dataset `VADeaths`.
 ## here is a regular code comment, that will remain as such
